5e32ef26ea09ed34e30e4318462cc48431457551
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2017-12-19 17:38:39 +0700

    Added requirements + Used template

--- investment_dashboard/portfolio/views.py
+++ investment_dashboard/portfolio/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, simple http")
+    template = loader.get_template('portfolio/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
