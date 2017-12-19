5c73ee1006d5c0ce087b03f10aff567bf69f1b39
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2017-12-19 16:35:13 +0700

    Setup PostgreSQL

--- investment_dashboard/investment_dashboard/settings.py
+++ investment_dashboard/investment_dashboard/settings.py
@@ -74,9 +74,15 @@ WSGI_APPLICATION = 'investment_dashboard.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'investment_dashboard',
+        'USER': 'investment_dashboard_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
